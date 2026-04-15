a0e09d3e804e2e817855357eda87872f0b021d48
Suraj Singh <suraj14mk@gmail.com>
2026-04-15 15:00:17 +0530

    Add Vercel frontend URL to CORS allowed origins

--- main.py
+++ main.py
@@ -53,6 +53,7 @@ app.add_middleware(
         "http://127.0.0.1:5173",
         "http://localhost:5174",
         "http://127.0.0.1:5174",
+        "https://frontend-six-nu-77.vercel.app",
         os.getenv("FRONTEND_URL", ""),
     ],
     allow_credentials=True,
