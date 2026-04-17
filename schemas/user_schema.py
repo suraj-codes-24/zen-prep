60ec94fd6a1a788e1dcc93d9d679fdc11b4834d3
Suraj Singh <suraj14mk@gmail.com>
2026-04-17 15:06:42 +0530

    Fix Google OAuth: make password optional in UserRegister schema

--- schemas/user_schema.py
+++ schemas/user_schema.py
@@ -4,7 +4,7 @@ from typing import Optional
 class UserRegister(BaseModel):
     name: str = Field(min_length=2, max_length=100)
     email: EmailStr
-    password: str = Field(min_length=6, max_length=128)
+    password: Optional[str] = Field(None, min_length=6, max_length=128)
     branch: Optional[str] = None
     year: Optional[int] = None
 
